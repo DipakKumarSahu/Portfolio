a7a9e9c106a5fced24fee823ae2e3e039a3ff41d
DipakLearnGitGitHub <ankitsahu77889900@gmail.com>
2025-07-24 23:59:08 +0530

    v1.1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import Tilt from "react-parallax-tilt";
-import profileImage from "../../assets/passphoto.jpeg";
+import profileImage from "../../../public/profilepic/passphoto.jpeg";
 
 const About = () => {
   return (
--- src/constants.js
+++ src/constants.js
@@ -1,35 +1,35 @@
 // Skills Section Logo's
-import htmlLogo from "./assets/tech_logo/html.png";
-import cssLogo from "./assets/tech_logo/css.png";
+import htmlLogo from "../public/assets/tech_logo/html.png";
+import cssLogo from "../public/assets/tech_logo/css.png";
 
-import javascriptLogo from "./assets/tech_logo/javascript.png";
-import reactjsLogo from "./assets/tech_logo/reactjs.png";
-import JspLogo from "./assets/tech_logo/jsplogo.png";
+import javascriptLogo from "../public/assets/tech_logo/javascript.png";
+import reactjsLogo from "../public/assets/tech_logo/reactjs.png";
+import JspLogo from "../public/assets/tech_logo/jsplogo.png";
 
-import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import tailwindcssLogo from "../public/assets/tech_logo/tailwindcss.png";
 
-import springbootLogo from "./assets/tech_logo/springboot.png";
-import Maven from "./assets/tech_logo/maven.png";
-import nodejsLogo from "./assets/tech_logo/nodejs.png";
-import ThymeleafLogo from "./assets/tech_logo/thymeleaf.png";
-import SpringMVCLogo from "./assets/tech_logo/Springmvc.png";
+import springbootLogo from "../public/assets/tech_logo/springboot.png";
+import Maven from "../public/assets/tech_logo/maven.png";
+import nodejsLogo from "../public/assets/tech_logo/nodejs.png";
+import ThymeleafLogo from "../public/assets/tech_logo/thymeleaf.png";
+import SpringMVCLogo from "../public/assets/tech_logo/Springmvc.png";
 
-import mysqlLogo from "./assets/tech_logo/mysql.png";
-import JPAAndHIbernate from "./assets/tech_logo/JPA & HIbernate.png";
+import mysqlLogo from "../public/assets/tech_logo/mysql.png";
+import JPAAndHIbernate from "../public/assets/tech_logo/JPA & HIbernate.png";
 
-import cLogo from "./assets/tech_logo/c.png";
-import cppLogo from "./assets/tech_logo/cpp.png";
-import javaLogo from "./assets/tech_logo/java.png";
-import postmanLogo from "./assets/tech_logo/postman.png";
-import JWTLogo from "./assets/tech_logo/jwt.png";
+import cLogo from "../public/assets/tech_logo/c.png";
+import cppLogo from "../public/assets/tech_logo/cpp.png";
+import javaLogo from "../public/assets/tech_logo/java.png";
+import postmanLogo from "../public/assets/tech_logo/postman.png";
+import JWTLogo from "../public/assets/tech_logo/jwt.png";
 
-import gitLogo from "./assets/tech_logo/git.png";
-import githubLogo from "./assets/tech_logo/github.png";
+import gitLogo from "../public/assets/tech_logo/git.png";
+import githubLogo from "../public/assets/tech_logo/github.png";
 
-import figmaLogo from "./assets/tech_logo/figma.png";
+import figmaLogo from "../public/assets/tech_logo/figma.png";
 
 // Education Section Logo's
-import bsaLogo from "./assets/education_logo/college.png";
+import bsaLogo from "../public/assets/education_logo/college.png";
 
 
 export const SkillsInfo = [
