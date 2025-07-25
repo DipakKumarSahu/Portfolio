e84b1b66ad266300b14f0eefcf5abe68bf6a7b39
DipakLearnGitGitHub <ankitsahu77889900@gmail.com>
2025-07-25 14:27:39 +0530

    v2.0

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
 
           {/* Resume Button  */}
           <a
-            href="https://docs.google.com/document/d/1jWZV10V1Mgef5_O9l1BONGkcelY3oVlU/edit?usp=drive_link&ouid=103694787736400739667&rtpof=true&sd=true"
+            href="https://drive.google.com/file/d/1yFhd2wQ_C64C--53If3vW4uglmkZW8YO/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 "
